8329903b4f103b9de31eef619891e1817e7bbf8e
Alex Cooksey <aecooksey2651@gmail.com>
2021-12-14 15:41:53 -0500

    commit tree 83fba8cb6cc9007063e7c0fd377016ef935614b7

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replaceAll(/[^A-z0-9' -]|\^/g, '')
+  }
+  static titleize(string) {
+    let array = string.split(' ')
+    array[0] = array[0].charAt(0).toUpperCase() + array[0].slice(1)
+    const dontCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    for (let i = 1; i < array.length; i++) {
+      if (dontCap.includes(array[i])) {
+        array[i] = array[i].charAt(0).toLowerCase() + array[i].slice(1)
+      } else {
+        array[i] = array[i].charAt(0).toUpperCase() + array[i].slice(1)
+      }
+    }
+    const newString = array.join(' ')
+    return newString
+  }
 }
